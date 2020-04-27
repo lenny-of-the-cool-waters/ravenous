a4f9474a9c372eef920cc88279bffadcc2335eb7
Lenny Git <lennygith@gmail.com>
2020-04-27 05:58:49 +0300

    Redid informatin passing structure btwn components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,37 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+//business object
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+//businesses array
+let businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
+//App component class
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
           <SearchBar />
-          <BusinessList />
+          <BusinessList businesses={businesses}/>
       </div>
     );
   }
