2c8e41735299a034c351d454964f54964ead3fb2
Lenny Git <lennygith@gmail.com>
2020-04-29 23:58:31 +0300

    Intergration to YelpApi

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,36 +3,24 @@ import './App.css';
 //import logo from '../../logo.svg';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../util/Yelp';
 
-//business object
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
-//businesses array
-let businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
 
 //App component class
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses: [],
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
   //method to provide functionality to 'Lets go' button
   searchYelp(term, location, sortBy){
     //simulate searching Yelp app. will be updated in future edits
-    console.log(`Searching Yelp with ${term}, ${location} and ${sortBy}`);
+    Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+      this.setState({businesses: businesses});
+    }); 
   }
 
   render() {
@@ -40,7 +28,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
           <SearchBar searchYelp={this.searchYelp}/>
-          <BusinessList businesses={businesses}/>
+          <BusinessList businesses={this.state.businesses}/>
       </div>
     );
   }
